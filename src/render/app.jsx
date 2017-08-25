7f291ce57817ab902f79bfac53e9c459b083daa6
minaduking <minaduki.supernova@gmail.com>
2017-08-25 18:38:36 +0900

    first commit

---
+++ src/render/app.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import HelloWorld from './helloworld';
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById("app")
+);
